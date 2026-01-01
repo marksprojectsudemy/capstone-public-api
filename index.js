55eb7d34a40f1b046647fa95294612d4709b0ee2
Mark <udemy.for.mark@gmail.com>
2026-01-01 20:42:25 +0100

    Implement API functionality

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/spells", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL + "spells?level=1");
+        console.log(JSON.stringify(result.data));
+        res.render("spells.ejs");
+    } catch(e) {
+        res.render("spells.ejs", { content: JSON.stringify(e.response.data) });
+    }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
